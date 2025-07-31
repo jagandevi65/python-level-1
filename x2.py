3732128dbb4d679d97173fa3e5a914b4dc64a8d9
jagandevi65 <jagandevi2006@gmail.com>
2025-07-31 10:56:07 +0530

    jagan

---
+++ x2.py
@@ -0,0 +1,14 @@
+print("Takshasnila university")
+print("ongur,tindivanam,villupuram dist.TN")
+print("-----------------------------------")
+print("STUDENT MARK LIST")
+print("--------------------------------")
+str1 =input("enter the registor no:")
+str2 = input("enter the name:")
+python =int (input("python mark:"))
+DBMS= int(input("DBMS mark:"))
+accounting= int (input("accounting mark:"))
+Total= python+DBMS+accounting
+print("total mark", Total)
+average = Total/3
+print("average mark:",average)
