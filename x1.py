1f50f7b7b4e503e15fb2356831ddb4998fe22869
jagandevi65 <jagandevi2006@gmail.com>
2025-07-31 10:53:47 +0530

    jaghan

---
+++ x1.py
@@ -0,0 +1,15 @@
+print("ARITHEMATIC OPERATIONS")
+print("----------------------")
+a= int (input("Enter the frist number:"))
+b= int (input("Enter the second number:"))
+print("result:")
+c=a+b
+print("result:",c)
+c=a-b
+print("sub:",c)
+c=a*b
+print("mul:",c)
+c=a/b
+print("div:",c)
+c=a%b
+print("mod:",c)
