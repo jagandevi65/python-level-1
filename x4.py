e4ae567b7c209293d7ce5c3bc944816074e80126
jagandevi65 <jagandevi2006@gmail.com>
2025-07-31 10:59:11 +0530

    jagan

---
+++ x4.py
@@ -0,0 +1,13 @@
+print( "jagan Supermarket")
+print("NO. 44, Nehru street, Puducherry")
+print("------------------------------")
+print(" Bill Receipt")
+print("------------------------------")
+str1 = input("Enter the serial Number: ")
+str2 = input (" Enter the portticatars: ")
+Rate = int (input ("Enthar the Rate: "))
+Quantity = int (input ("Enter the Quantity:"))
+Total= Rate *Quantity
+print (" Total Amount Rs:", Total) 
+GST = Total *10/100
+print ("GS(10 per);", GST)
